18216b239b0a4c9e9f3532b540aa2521586911ef
JASPMobus <mobusj@live.com>
2020-05-01 08:08:09 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+
+    def index
+        @cart = session[:cart]
+    end
+
+    def add
+        product = params[:product]
+
+        cart << product
+
+        session[:cart] = cart
+
+        redirect_to products_path
+    end
+end
